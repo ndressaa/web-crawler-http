79b3e220c0d2bd1cf408fe6f98d0188e012cd35a
andressa <andressajucoski@gmail.com>
2025-03-08 19:46:48 -0300

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,14 +1,37 @@
-function normalizeURL(urlString) {
-  const urlObject = new URL(urlString);
-  const hostpath = urlObject.hostname + urlObject.pathname;
+const { JSDOM } = require("jsdom");
 
-  if (hostpath.length > 0 && hostpath.endsWith("/")) {
-    return hostpath.slice(0, -1);
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const aElements = dom.window.document.querySelectorAll("a");
+  for (const aElement of aElements) {
+    if (aElement.href.slice(0, 1) === "/") {
+      try {
+        urls.push(new URL(aElement.href, baseURL).href);
+      } catch (err) {
+        console.log(`${err.message}: ${aElement.href}`);
+      }
+    } else {
+      try {
+        urls.push(new URL(aElement.href).href);
+      } catch (err) {
+        console.log(`${err.message}: ${aElement.href}`);
+      }
+    }
   }
+  return urls;
+}
 
-  return hostpath;
+function normalizeURL(url) {
+  const urlObj = new URL(url);
+  let fullPath = `${urlObj.host}${urlObj.pathname}`;
+  if (fullPath.length > 0 && fullPath.slice(-1) === "/") {
+    fullPath = fullPath.slice(0, -1);
+  }
+  return fullPath;
 }
 
 module.exports = {
   normalizeURL,
+  getURLsFromHTML,
 };
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require("./crawl");
+const { normalizeURL, getURLsFromHTML } = require("./crawl");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL strip protocol", () => {
@@ -27,4 +27,43 @@ test("normalizeURL strip http", () => {
   const actual = normalizeURL(input);
   const expected = "blog.boot.dev/path";
   expect(actual).toBe(expected);
+});
+
+test("getURLsFromHTML absolute", () => {
+  const inputURL = "https://blog.boot.dev";
+  const inputBody =
+    '<html><body><a href="https://blog.boot.dev"><span>Boot.dev></span></a></body></html>';
+  const actual = getURLsFromHTML(inputBody, inputURL);
+  const expected = ["https://blog.boot.dev/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML relative", () => {
+  const inputURL = "https://blog.boot.dev";
+  const inputBody =
+    '<html><body><a href="/path/one"><span>Boot.dev></span></a></body></html>';
+  const actual = getURLsFromHTML(inputBody, inputURL);
+  const expected = ["https://blog.boot.dev/path/one"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML both", () => {
+  const inputURL = "https://blog.boot.dev";
+  const inputBody =
+    '<html><body><a href="/path/one"><span>Boot.dev></span></a><a href="https://other.com/path/one"><span>Boot.dev></span></a></body></html>';
+  const actual = getURLsFromHTML(inputBody, inputURL);
+  const expected = [
+    "https://blog.boot.dev/path/one",
+    "https://other.com/path/one",
+  ];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML handle error", () => {
+  const inputURL = "https://blog.boot.dev";
+  const inputBody =
+    '<html><body><a href="path/one"><span>Boot.dev></span></a></body></html>';
+  const actual = getURLsFromHTML(inputBody, inputURL);
+  const expected = [];
+  expect(actual).toEqual(expected);
 });
